e1c35347f100f02b0c84e0bf0314801ed4f38200
Luis Cortes <MrLuisCortes@gmail.com>
2025-07-16 21:52:01 -0600

    Expand ACTIVESEARCH user list with 24 diverse names including Hispanic names and varied email domains

--- ACTIVESEARCH/myapp.py
+++ ACTIVESEARCH/myapp.py
@@ -1,16 +1,11 @@
 import flask
 
-# Server 3 urls:
-# /static for /static/js/htmx.js /static/img/bars.svg 
-# / or /index.html 
-# /search/
-
 app = flask.Flask(__name__, static_url_path='/static')
 
 class User:
     id = 0
     def __init__(self, fname, lname, email ):
-        User.id  += 1
+        User.id  +=1
         self.id = User.id 
         self.fname = fname
         self.lname = lname 
@@ -23,33 +18,47 @@ class User:
         return word.lower() in all.lower()
 
 users = [
-    User("abe", "vida", "abe@nowhere.com"),
-    User("betty", "b", "bb@nowhere.com"),
-    User("joe", "robinson", "jrobinson@nowhere.com"),
+    User("John", "Smith", "jsmith@company.com"),
+    User("Jane", "Doe", "jane.doe@email.org"),
+    User("Bob", "Johnson", "bob.j@tech.net"),
 
-    User("Luis", "Cortes", "Luis@somewhere.com"),
-    User("marty", "hinkle", "mhinkle@nowhere.com"),
-    User("matthew", "robinson", "mrobinson@nowhere.com"),
+    User("Maria", "Garcia", "mgarcia@business.com"),
+    User("Carlos", "Rodriguez", "crodriguez@work.org"),
+    User("Diana", "Davis", "ddavis@corp.net"),
 
-    User("collin", "western", "cwest@nowhere.com"),
-    User("marty", "hinkle II", "mhinkle2@nowhere.com"),
-    User("joe", "robinson", "jrobinson@nowhere.com"),
+    User("Edward", "Miller", "emiller@office.com"),
+    User("Sofia", "Martinez", "smartinez@company.org"),
+    User("George", "Taylor", "gtaylor@business.net"),
 
-    User("juan", "vida", "juanvida@nowhere.com"),
-    User("marty", "hinkle III", "mhinkle3@nowhere.com"),
-    User("zoe", "omega", "zoe@nowhere.com") 
+    User("Helen", "Anderson", "handerson@work.com"),
+    User("Jose", "Lopez", "jlopez@email.org"),
+    User("Julia", "Jackson", "jjackson@corp.com"),
+
+    User("Ana", "Gonzalez", "agonzalez@enterprise.com"),
+    User("David", "Wilson", "dwilson@startup.net"),
+    User("Isabella", "Hernandez", "ihernandez@agency.org"),
+
+    User("Michael", "Brown", "mbrown@consulting.com"),
+    User("Elena", "Torres", "etorres@studio.net"),
+    User("Robert", "Jones", "rjones@partners.org"),
+
+    User("Carmen", "Flores", "cflores@creative.com"),
+    User("Thomas", "Moore", "tmoore@innovate.net"),
+    User("Patricia", "Rivera", "privera@design.org"),
+
+    User("Christopher", "Lee", "clee@venture.com"),
+    User("Gabriela", "Cruz", "gcruz@dynamic.net"),
+    User("Daniel", "White", "dwhite@strategic.org")
 ]
 
-@app.route("/")
-@app.route("/index.html")
+@app.route('/index.html')
 def root():
     return flask.render_template("index.html")
 
 
 @app.route('/search/', methods=['POST'])
 def search():
-    templ = """
-            {% for user in users %}
+    templ =             {% for user in users %}
             <tr>
                 <td>{{ user.id }}</td>
                 <td>{{ user.fname }}</td>
@@ -57,11 +66,10 @@ def search():
                 <td>{{ user.email }}</td>
             </tr>
             {% endfor %}
-    """
     searchWord = flask.request.form.get('search', None)
     matchusers = [user for user in users if user.search(searchWord)]
     return flask.render_template_string(templ,users=matchusers)
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(debug = True) 
